9d6d2fb9547d2c99df946530e67b35f3dd048426
Remmy Clay <remmy.lauren@gmail.com>
2017-11-04 07:40:27 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -184,6 +184,9 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
       board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 
+      output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
+      
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
