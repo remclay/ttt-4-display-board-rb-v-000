e324e4ef01b674ed8535bbf401068c54f1539241
Remmy Clay <remmy.lauren@gmail.com>
2017-11-04 07:25:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board (current_board)
+  puts " #{current_board[0]} | #{current_board[1]} | #{current_board[2]} "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
