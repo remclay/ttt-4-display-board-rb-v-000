f5e0d03fcc4b86b114d319c63717079a55910a1a
Remmy Clay <remmy.lauren@gmail.com>
2017-11-04 07:38:23 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -184,11 +184,11 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
       board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 
-      expect(rows[0]).to eq(" 0 | 0 | 0 ")
+      expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" 0 | 0 | 0 ")
+      expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" 0 | 0 | 0 ")
+      expect(rows[4]).to eq(" O | O | O ")
       # *** Comment the line below by adding a # at the line start ***
       #expect(true).to be(true)
     end
