ce97750a5d2cdb1858d02b8ebc16555c0792d004
Remmy Clay <remmy.lauren@gmail.com>
2017-11-04 07:26:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 def display_board (current_board)
   puts " #{current_board[0]} | #{current_board[1]} | #{current_board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{current_board[3]} | #{current_board[4]} | #{current_board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{current_board[6]} | #{current_board[7]} | #{current_board[8]} "
 end
